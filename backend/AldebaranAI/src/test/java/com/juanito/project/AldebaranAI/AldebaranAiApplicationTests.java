c962f64bbb54d3176a885c1a14c1edd510743fc1
juanitoaldebaran <juanitosiahaan1711@gmail.com>
2025-09-01 15:09:49 +0100

    add backend project

---
+++ backend/AldebaranAI/src/test/java/com/juanito/project/AldebaranAI/AldebaranAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanito.project.AldebaranAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AldebaranAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
