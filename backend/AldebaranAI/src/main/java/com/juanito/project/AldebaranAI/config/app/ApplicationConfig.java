1d303f31c9685a79659da8a45ced9620ebd1e986
juanitoaldebaran <juanitosiahaan1711@gmail.com>
2025-09-09 23:30:02 +0100

    addition on backend ai

--- backend/AldebaranAI/src/main/java/com/juanito/project/AldebaranAI/config/app/ApplicationConfig.java
+++ backend/AldebaranAI/src/main/java/com/juanito/project/AldebaranAI/config/app/ApplicationConfig.java
@@ -3,9 +3,7 @@ package com.juanito.project.AldebaranAI.config.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +13,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class ApplicationConfig {
@@ -49,10 +48,15 @@ public class ApplicationConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
+        String frontendUrl = System.getenv().getOrDefault(
+                "FRONTEND_URL",
+                "http://localhost:5173"
+        );
+
+        corsConfiguration.setAllowedOrigins(List.of(frontendUrl));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"));
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-        corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+        corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -60,6 +64,4 @@ public class ApplicationConfig {
 
         return source;
     }
-
-
 }
